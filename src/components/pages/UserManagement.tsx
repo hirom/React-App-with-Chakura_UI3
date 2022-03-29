97c3c97ffc6e0477dd88b44f0b4527aef259fce2
hirom <hirom0808@gmail.com>
2022-03-29 23:16:40 +0900

    Created User List component

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,16 @@
-import { Box, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap>
+    <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-        ></Box>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="hirom"
+          fullName="hirom-full"
+        />
       </WrapItem>
     </Wrap>
   );
